9e92cb53ef08c72804169da96c95fce10d5063e0
Anthony Shellman <atshellman@gmail.com>
2025-10-06 10:42:00 -0600

    add readiness endpoint, update fileserver path, refactor

--- main.go
+++ main.go
@@ -11,9 +11,9 @@ func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
-	fs := http.FileServer(http.Dir(filepathRoot))
 	mux := http.NewServeMux()
-	mux.Handle("/", fs)
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	server := &http.Server{
 		Addr: ":" + port,
@@ -24,4 +24,10 @@ func main() {
 	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		log.Fatalf("Could not listen on :%s: %v\n", port, err)
 	}
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
