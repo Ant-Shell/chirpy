c7739c4308e56e5f746b51a0bddcf95941885b8c
Anthony Shellman <atshellman@gmail.com>
2025-10-13 10:59:30 -0600

    move metrics related functions

--- main.go
+++ main.go
@@ -36,20 +36,5 @@ func main() {
 	}
 }
 
-func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	hits := cfg.fileserverHits.Load()
-	template := "<html>\n<body>\n<h1>Welcome, Chirpy Admin</h1>\n<p>Chirpy has been visited %d times!</p>\n</body>\n</html>"
-
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, template, hits)
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w,r)
-	})
-}
 
 
