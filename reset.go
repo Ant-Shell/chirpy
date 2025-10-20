a2e35865bf96167842f2f80ba59e473cc24c791f
Anthony Shellman <atshellman@gmail.com>
2025-10-20 13:17:10 -0600

    update messages

--- reset.go
+++ reset.go
@@ -4,17 +4,17 @@ import "net/http"
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 	if cfg.platform != "dev" {
-		respondWithError(w, http.StatusForbidden, "this action is forbidden", nil)
+		respondWithError(w, http.StatusForbidden, "Reset is only allowed in dev environment", nil)
 		return
 	}
 	err := cfg.db.Reset(r.Context())
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "unable to reset", err)
+		respondWithError(w, http.StatusInternalServerError, "Failed to reset the database", err)
 		return
 	}
 
 	cfg.fileserverHits.Store(0)
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+	w.Write([]byte("Hits reset to 0 and database reset to initial state"))
 }
