9486a52956d53747c179afc9f55b51b14e075fe0
Anthony Shellman <atshellman@gmail.com>
2025-10-20 13:08:39 -0600

    add user reset action

--- reset.go
+++ reset.go
@@ -3,6 +3,16 @@ package main
 import "net/http"
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	if cfg.platform != "dev" {
+		respondWithError(w, http.StatusForbidden, "this action is forbidden", nil)
+		return
+	}
+	err := cfg.db.Reset(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "unable to reset", err)
+		return
+	}
+
 	cfg.fileserverHits.Store(0)
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
