91a675a74586b47be96ac0cd22628749ba342694
Anthony Shellman <atshellman@gmail.com>
2025-10-25 11:23:35 -0600

    initial commit

---
+++ get_single_chirp.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+	"github.com/google/uuid"
+)
+
+func (cfg *apiConfig) handlerGetSingleChirp(w http.ResponseWriter, r *http.Request) {
+	chirpID := r.PathValue("chirpID")
+	parsedID, err := uuid.Parse(chirpID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to parse ID", err)
+	}
+
+	singleChirp, err := cfg.db.GetChirp(r.Context(), parsedID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Unable to locate chirp", err)
+	}
+
+	foundChirp := Chirp{
+		ID: singleChirp.ID,
+		Body: singleChirp.Body,
+		CreatedAt: singleChirp.CreatedAt,
+		UpdatedAt: singleChirp.UpdatedAt,
+		UserID: singleChirp.UserID,
+	}
+
+	respondWithJSON(w, http.StatusOK, foundChirp)
+}
