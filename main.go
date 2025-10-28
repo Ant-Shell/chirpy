e66f06d6af02010944458897416fb89ec86ba413
Anthony Shellman <atshellman@gmail.com>
2025-10-28 11:19:11 -0600

    add route and handler for login functionality

--- main.go
+++ main.go
@@ -68,6 +68,7 @@ func main() {
 	mux.HandleFunc("POST /admin/reset", cfg.handlerReset)
 	mux.HandleFunc("POST /api/chirps", cfg.handlerCreateChirp)
 	mux.HandleFunc("POST /api/users", cfg.handlerCreateUser)
+	mux.HandleFunc("POST /api/login", cfg.handleLogin)
 
 	server := &http.Server{
 		Addr: ":" + port,
