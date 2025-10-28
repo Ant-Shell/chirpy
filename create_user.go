a44b3c3266d5347ea9a3c6050905e8ed43a2481a
Anthony Shellman <atshellman@gmail.com>
2025-10-28 11:16:11 -0600

    modify handler to include password

--- create_user.go
+++ create_user.go
@@ -4,11 +4,15 @@ import (
 	"encoding/json"
 	"net/http"
 	"strings"
+
+	"github.com/Ant-Shell/chirpy/internal/auth"
+	"github.com/Ant-Shell/chirpy/internal/database"
 )
 
 func (cfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 			Email string `json:"email"`
+			Password string `json:"password"`
 	}
 
 	var p parameters
@@ -17,12 +21,27 @@ func (cfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 	p.Email = strings.TrimSpace(p.Email)
+	p.Password = strings.TrimSpace(p.Password)
 	if p.Email == "" {
 		respondWithError(w, http.StatusBadRequest, "email is required", nil)
 		return
 	}
 
-	dbUser, err := cfg.db.CreateUser(r.Context(), p.Email)
+	if p.Password == "" {
+		respondWithError(w, http.StatusBadRequest, "password is required", nil)
+		return
+	}
+
+	hash, err := auth.HashPassword(p.Password)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "could not hash password", err)
+		return
+	}
+
+	dbUser, err := cfg.db.CreateUser(r.Context(), database.CreateUserParams{
+		Email: p.Email,
+		HashedPassword: hash,
+	})
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "could not create user", err)
 		return
