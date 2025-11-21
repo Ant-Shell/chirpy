c09b3868cd10f925c05ac529dc0b8feb4f8ee938
Anthony Shellman <atshellman@gmail.com>
2025-11-20 18:48:52 -0700

    adjust error messages

--- reset.go
+++ reset.go
@@ -4,17 +4,17 @@ import "net/http"
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 	if cfg.platform != "dev" {
-		respondWithError(w, http.StatusForbidden, "Reset is only allowed in dev environment", nil)
+		respondWithError(w, http.StatusForbidden, "reset is only allowed in dev environment", nil)
 		return
 	}
 	err := cfg.db.Reset(r.Context())
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Failed to reset the database", err)
+		respondWithError(w, http.StatusInternalServerError, "failed to reset the database", err)
 		return
 	}
 
 	cfg.fileserverHits.Store(0)
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits reset to 0 and database reset to initial state"))
+	w.Write([]byte("hits reset to 0 and database reset to initial state"))
 }
