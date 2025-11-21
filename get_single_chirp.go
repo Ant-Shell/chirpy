9d50eb887bc6d521443f29d07e33269bc408b8aa
Anthony Shellman <atshellman@gmail.com>
2025-11-20 18:47:03 -0700

    adjust error messages

--- get_single_chirp.go
+++ get_single_chirp.go
@@ -9,12 +9,12 @@ func (cfg *apiConfig) handlerGetSingleChirp(w http.ResponseWriter, r *http.Reque
 	chirpID := r.PathValue("chirpID")
 	parsedID, err := uuid.Parse(chirpID)
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Unable to parse ID", err)
+		respondWithError(w, http.StatusInternalServerError, "unable to parse ID", err)
 	}
 
 	singleChirp, err := cfg.db.GetChirp(r.Context(), parsedID)
 	if err != nil {
-		respondWithError(w, http.StatusNotFound, "Unable to locate chirp", err)
+		respondWithError(w, http.StatusNotFound, "unable to locate chirp", err)
 	}
 
 	foundChirp := Chirp{
