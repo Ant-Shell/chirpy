2545ec57c419b68c3847ae5f0780f77c53208e19
Anthony Shellman <atshellman@gmail.com>
2025-11-29 15:04:43 -0700

    add query param and filtering by author_id

--- get_all_chrips.go
+++ get_all_chrips.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/google/uuid"
+)
 
 func (cfg *apiConfig) handlerGetAllChirps(w http.ResponseWriter, r *http.Request) {
 	dbChirpList, err := cfg.db.ListChirps(r.Context())
@@ -10,7 +14,6 @@ func (cfg *apiConfig) handlerGetAllChirps(w http.ResponseWriter, r *http.Request
 	}
 
 	chirpList := []Chirp{}
-
 	for _, chirp := range dbChirpList {
 		updatedChirp := Chirp{
 			ID: chirp.ID,
@@ -22,5 +25,24 @@ func (cfg *apiConfig) handlerGetAllChirps(w http.ResponseWriter, r *http.Request
 		chirpList = append(chirpList, updatedChirp)
 	}
 
-	respondWithJSON(w, http.StatusOK, chirpList)
+	authorIDString := r.URL.Query().Get("author_id")
+	if authorIDString == "" {
+		respondWithJSON(w, http.StatusOK, chirpList)
+		return
+	}
+
+	parsedID, err := uuid.Parse(authorIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "invalid author_id", err)
+		return
+	}
+
+	targetedChirpList := []Chirp{}
+	for _, targetedChirp := range chirpList {
+		if targetedChirp.UserID == parsedID {
+			targetedChirpList = append(targetedChirpList, targetedChirp)
+		}
+	}
+
+	respondWithJSON(w, http.StatusOK, targetedChirpList)
 }
